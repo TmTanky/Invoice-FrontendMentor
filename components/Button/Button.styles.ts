4a069929cd63502f9ed6ebd24b0b613cded62c39
TmLololol <tmtanky@gmail.com>
2022-01-24 23:40:12 +0800

    added fonts

--- components/Button/Button.styles.ts
+++ components/Button/Button.styles.ts
@@ -1,9 +1,24 @@
 import styled from 'styled-components'
+import { darkTheme } from '../../styles/theme/theme'
 
 type ButtonProps = {
   status: 'paid' | 'pending' | 'draft'
 }
 
+export const themeSwitcher = (type?: string) => {
+  if (type === 'bg') {
+    return darkTheme.bgTransparent
+  }
+  return darkTheme.bgStatus
+}
+
 export const Button = styled.button<ButtonProps>`
-    background-color: ${({ })};
+  background-color: ${({ status }) => themeSwitcher('bg')[status]};
+  color: ${({ status }) => themeSwitcher()[status]};
+  outline: none;
+  border: none;
+  min-width: 80px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-weight: 700;
 `
