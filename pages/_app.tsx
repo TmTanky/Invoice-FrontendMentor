1eaf29aa4899249bf72d89f503d547c92fcd396a
TmLololol <tmtanky@gmail.com>
2022-03-27 06:24:21 +0800

    done saving theme and some adjustments to remove warnings in console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import '../styles/globals.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { ToastContainer } from 'react-toastify'
 import { Layout } from '@/components/Layout'
+import { useIsomorphicLayoutEffect } from '@/hooks/useIsomorphicLayoutEffect'
 import { FormProvider } from 'contexts'
 import { darkTheme, lightTheme } from '../styles/theme/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState('light')
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const localTheme = localStorage.getItem('theme')
     if (!localTheme) {
       localStorage.setItem('theme', 'light')
